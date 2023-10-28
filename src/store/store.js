75be15b0232863b53daebd4a6228fe59a8dd5d9b
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-10-27 21:12:24 -0700

    RTK Query

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore} from '@reduxjs/toolkit';
 import {counterSlice} from './slices/counter';
 import { pokemonSlice } from './slices/pokemon';
+import { todosApi } from './apis';
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
-    pokemons: pokemonSlice.reducer
+    pokemons: pokemonSlice.reducer,
+    // LLAVES CUADRADAS = PROPIEDAD COMPUTADA
+    [todosApi.reducerPath]: todosApi.reducer,
   },
+  // UN MIDDLEWARE ES UNA FUNCION QUE SE EJECUTA ANTES QUE OTRA
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    .concat(todosApi.middleware)
 })
